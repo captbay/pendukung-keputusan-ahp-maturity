9a3ee5b43be4cda3b0ab348b4a28e78a5b734af1
clvnand <calvinandrean456@gmail.com>
2024-07-18 22:59:54 +0700

    commenting all of the dashboard-admin

--- app/(admin)/dashboard-admin/page.tsx
+++ app/(admin)/dashboard-admin/page.tsx
@@ -4,16 +4,17 @@ import DashboardUiAdmin from "@/app/ui/dashboard-ui-admin/dashboardUiAdmin";
 import { getAhpData, getAllUser, getQuestionMaturityAdmin, getResultMaturityAll } from "@/lib/actions";
 
 export default async function DashboardAdmin() {
-  const session = await auth();
-  const user = await getAllUser();
-  const ahpResult = await getAhpData();
-  const maturityResult = await getResultMaturityAll();
+  // const session = await auth();
+  // const user = await getAllUser();
+  // const ahpResult = await getAhpData();
+  // const maturityResult = await getResultMaturityAll();
   return (
-    <DashboardUiAdmin 
-      session={session} 
-      data={user!.data}
-      ahpResult={ahpResult!.data}
-      maturityResult={maturityResult!.data}
-    />
+    <></>
+    // <DashboardUiAdmin 
+    //   session={session} 
+    //   data={user!.data}
+    //   ahpResult={ahpResult!.data}
+    //   maturityResult={maturityResult!.data}
+    // />
   );
 }
